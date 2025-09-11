848d25fd934054af1c9b15fd223f18e2bd3bee72
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-11 14:02:18 +0200

    Kalkalugaiu xml-a eginda

--- Section3AndroidFoundationsPart1XMLLayouts/app/src/main/java/com/example/section3androidfoundations_part1xmllayouts/MainActivity.java
+++ Section3AndroidFoundationsPart1XMLLayouts/app/src/main/java/com/example/section3androidfoundations_part1xmllayouts/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.section3androidfoundations_part1xmllayouts;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView erantzuna= findViewById(R.id.Text_View);
+
+        RadioButton radio_Hombre= findViewById(R.id.radio_button_Hombre);
+        RadioButton radio_Mujer= findViewById(R.id.radio_button_Mujer);
+
+        EditText editText_edad= findViewById(R.id.edit_text_Edad);
+        EditText editText_pies= findViewById(R.id.edit_text_Pies);
+        EditText editText_pulgadas= findViewById(R.id.edit_text_Pulgadas);
+        EditText editText_peso= findViewById(R.id.edit_text_Peso);
+
+        Button button_calcular= findViewById(R.id.button_Calcular);
     }
 }
