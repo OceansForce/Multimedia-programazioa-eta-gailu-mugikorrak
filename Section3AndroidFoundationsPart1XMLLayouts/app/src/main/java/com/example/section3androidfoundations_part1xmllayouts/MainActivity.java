7ce20857be35d148c7501544052b774f4bbf2570
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-12 10:01:11 +0200

    4. Atala bucatzen

--- Section3AndroidFoundationsPart1XMLLayouts/app/src/main/java/com/example/section3androidfoundations_part1xmllayouts/MainActivity.java
+++ Section3AndroidFoundationsPart1XMLLayouts/app/src/main/java/com/example/section3androidfoundations_part1xmllayouts/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.section3androidfoundations_part1xmllayouts;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -14,21 +15,59 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView erantzuna;
+    private RadioButton radio_Hombre;
+    private RadioButton radio_Mujer;
+    private EditText editText_edad;
+    private EditText editText_pies;
+    private EditText editText_pulgadas;
+    private EditText editText_peso;
+
+    private Button button_calcular;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView erantzuna= findViewById(R.id.Text_View);
+        elementuak_aurkitu();
 
-        RadioButton radio_Hombre= findViewById(R.id.radio_button_Hombre);
-        RadioButton radio_Mujer= findViewById(R.id.radio_button_Mujer);
 
-        EditText editText_edad= findViewById(R.id.edit_text_Edad);
-        EditText editText_pies= findViewById(R.id.edit_text_Pies);
-        EditText editText_pulgadas= findViewById(R.id.edit_text_Pulgadas);
-        EditText editText_peso= findViewById(R.id.edit_text_Peso);
+    }
 
-        Button button_calcular= findViewById(R.id.button_Calcular);
+    private void elementuak_aurkitu(){
+        erantzuna= findViewById(R.id.Text_View);
+
+        radio_Hombre= findViewById(R.id.radio_button_Hombre);
+        radio_Mujer= findViewById(R.id.radio_button_Mujer);
+
+        editText_edad= findViewById(R.id.edit_text_Edad);
+        editText_pies= findViewById(R.id.edit_text_Pies);
+        editText_pulgadas= findViewById(R.id.edit_text_Pulgadas);
+        editText_peso= findViewById(R.id.edit_text_Peso);
+
+        button_calcular= findViewById(R.id.button_Calcular);
+    }
+
+    private void setButton_listener(){
+        button_calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculo();
+            }
+        });
+    }
+
+    private void calculo(){
+        int edad= Integer.parseInt(editText_edad.getText().toString());
+        int pies= Integer.parseInt(editText_pies.getText().toString());
+        int pulgadas= Integer.parseInt(editText_pulgadas.getText().toString());
+        int peso= Integer.parseInt(editText_peso.getText().toString());
+
+        int pulgadas_totales= (pies*12)+pulgadas;
+
+        double altura_a_metros=pulgadas_totales*0.0254;
+
+        double bmi= peso/ (altura_a_metros*pulgadas_totales);
     }
 }
